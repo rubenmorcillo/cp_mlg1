8c0ecee0033566f91a3f2cfdccce92f9111b364d
rubenmorcillo <ruben.morcillo.005@estudiante.iesoretania.es>
2019-06-08 19:48:10 +0200

    Autenticar usuarios de la base de datos

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,13 +5,14 @@ namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 
 /**
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  * @ORM\Table(name="User")
  */
-class User
+class User implements UserInterface
 {
 
     /**
@@ -127,7 +128,7 @@ class User
     /**
      * @return String
      */
-    public function getLogin()
+    public function getUsername()
     {
         return $this->login;
     }
@@ -331,4 +332,24 @@ class User
     }
 
 
+    public function getRoles()
+    {
+        $roles = ['ROLE_PLAYER'];
+
+        if ($this->isEsAdmin()){
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        return $roles;
+    }
+
+    public function getSalt()
+    {
+       return null;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
 }
